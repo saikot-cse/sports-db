496a8a867175a2bf436412ea4c5beefa6f2642bd
Saifullah Mansur <58740153+saikot-cse@users.noreply.github.com>
2022-02-28 05:07:53 +0600

    Player card updated

--- js/sports.js
+++ js/sports.js
@@ -1,8 +1,27 @@
-const allPlayers = () =>{
+const allPlayers = () => {
   const searchPlayer = document.getElementById(`search-input`).value;
-  const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchPlayer}`
-  console.log(url);
+  const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchPlayer}`;
   fetch(url)
-  .then(res => res.json())
-  .then(data => console.log(data));
-}
+    .then((res) => res.json())
+    .then((data) => showPlayerDetails(data.player));
+};
+
+const showPlayerDetails = (players) => {
+  players.forEach((player) => {
+    const playerCard = document.getElementById(`player-container`);
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="player-card border p-5 my-5">
+  <div class="profile-picture">
+    <img src="${player.strThumb}" alt="" class="w-50 rounded">
+  </div>
+  <h2>Name: ${player.strPlayer}</h2>
+  <h5>Country: ${player.strNationality}</h5>
+  <p>${player.strDescriptionEN.slice(0,150)}</p>
+  <div class="remove-details-button">
+    <button class="btn btn-danger">Remove</button>
+    <button class="btn btn-success">Details</button>
+  </div>
+</div>`;
+    playerCard.appendChild(div);
+  });
+};
